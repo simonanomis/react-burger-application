86364c6c277e602545faeffeea8cb45a2f82e5b5
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2021-10-29 23:21:01 +0200

    remove ingredients safely

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -15,7 +15,9 @@ const buildControls = (props) => (
             <BuildControl
                 key={control.label}
                 label={control.label}
-                added={() => props.ingredientAdded(control.type)}/>
+                added={() => props.ingredientAdded(control.type)}
+                removed={() => props.ingredientRemoved(control.type)}
+                disabled={props.disabled[control.type]}/>
         ))}
     </div>
 );
