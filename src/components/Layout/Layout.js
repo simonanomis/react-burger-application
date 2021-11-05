b7dd4f0140304b039c2c5bc6c35925744865ac12
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2021-11-05 23:43:43 +0100

    add responsive sidedrawer for mobile devices

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = (props) => (
     <Auxiliary>
-        <Toolbar/>
+        <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
