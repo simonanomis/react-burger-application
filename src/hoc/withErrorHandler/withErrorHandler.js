256c91f43111577455d651486111be366c2f2ed9
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2021-11-23 20:52:21 +0100

    remove old interceptors and prevent memory leaks

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,13 +9,20 @@ const withErrorHandler = (WrappedComponent, axios) => {
         }
 
         componentWillMount() {
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             })
-            axios.interceptors.response.use(res => res,  error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res,  error => {
                 this.setState({error: error});
             });
+        }
+
+        //prevent memory leaks
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
+
         }
 
         errorConfirmedHandler = () => {
