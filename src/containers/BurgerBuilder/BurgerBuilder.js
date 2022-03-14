b30aeff91e1f40cf52e92b5921efdc5185529db7
Simona Ristovska <s.ristovska@linkplus-it.com>
2022-03-14 13:55:29 +0100

    passing ingredients via query params

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -71,7 +71,18 @@ class BurgerBuilder extends Component {
         //         console.log(error)
         //         this.setState({loading: false, isOrdering: false});
         //     });
-        this.props.history.push('/checkout');
+        const queryParams = [];
+        for(let i in this.state.ingredients) {
+            queryParams.push(
+                encodeURIComponent(i) + 
+                '=' + 
+                encodeURIComponent(this.state.ingredients[i]));
+        }
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
     updatePurchaseState (ingredients) {
