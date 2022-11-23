74506b2d028824af9f004cc7f7490182a03e6518
Simona Ristovska <simona.ristovska@blackboard.com>
2022-11-24 00:03:20 +0100

    getting a token from the backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,6 @@
 import * as actionTypes from "./actionTypes";
 import { error } from "autoprefixer/lib/utils";
+import axios from "axios";
 
 export const authStart = () => {
   return {
@@ -22,7 +23,26 @@ export const authFail = (error) => {
 };
 
 export const auth = (email, password) => {
+  const authData = {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  };
+
   return (dispatch) => {
     dispatch(authStart());
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCGjYq1tN3AMg3ng47xz-bFzkbtOQymbqQ",
+        authData
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(authFail(error));
+      });
   };
 };
