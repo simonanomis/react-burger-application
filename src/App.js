3d0348f64b64e1af1f4a9bfb5aab448f869d4fb9
Simona Ristovska <simona.ristovska@blackboard.com>
2022-11-23 22:54:09 +0100

    Adding auth form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 import { Route, Switch } from "react-router-dom";
 import Orders from "./containers/Orders/Orders";
+import { Auth } from "./containers/Auth/Auth";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={BurgerBuilder} />
             <Route path="/checkout" component={Checkout} />
+            <Route path="/auth" component={Auth} />
             <Route path="/orders" component={Orders} />
           </Switch>
         </Layout>
