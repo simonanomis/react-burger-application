31190b0a14082ec3376157919c3d495842bfdf8c
Simona Ristovska <simona.ristovska@blackboard.com>
2022-11-25 11:57:15 +0100

    adding logout func

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,20 @@ export const authFail = (error) => {
   };
 };
 
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT,
+  };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
+
 export const auth = (email, password, isSignUp) => {
   const authData = {
     email: email,
@@ -43,6 +57,7 @@ export const auth = (email, password, isSignUp) => {
       .then((response) => {
         console.log(response);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch((error) => {
         console.log(error);
