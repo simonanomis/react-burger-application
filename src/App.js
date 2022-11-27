fc199dc80d89db8b2cd70860b0d8ba908bb4c0fc
Simona Ristovska <simona.ristovska@blackboard.com>
2022-11-27 23:15:13 +0100

    fix visual bugs

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends Component {
           <Route path="/" exact component={BurgerBuilder} />
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth} />
           <Route path="/logout" component={Logout} />
           <Redirect to="/" />
         </Switch>
