b8c76550d17cfb5cf1614f965be1b76cd91be20c
Simona Ristovska <simona.ristovska@blackboard.com>
2022-11-27 12:48:17 +0100

    persisting auth state with localStorage and if user is signed in on refresh it stays signed in

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -10,4 +10,4 @@ export {
   fetchOrders,
 } from "./orderActions";
 
-export { auth, logout, setAuthRedirectPath } from "./auth";
+export { auth, logout, setAuthRedirectPath, authCheckState } from "./auth";
